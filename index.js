7259a2b79075b48cd1b2361fc825d9acb63cce3f
Lolita <lolita.danancher@wanadoo.fr>
2022-09-19 15:39:18 +0200

    1er envoi

---
+++ index.js
@@ -0,0 +1,31 @@
+const countdownDisplay = document.querySelector(".countdownDisplay") 
+let totalSeconds;
+let interval;
+
+function countDown() {
+    const minutes = Math.floor(totalSeconds / 60);
+    const seconds = totalSeconds % 60;
+
+    let sec = seconds < 10 ? "0" + seconds : seconds
+
+    countdownDisplay.textContent = minutes + " : " + sec;
+
+    if (totalSeconds > 0) {
+        totalSeconds--;
+    } else {
+        countdownDisplay.textContent = "C'est terminé !"
+        clearInterval(interval);
+    }
+}
+start.addEventListener("click", (e) => {
+    e.preventDefault();
+    if(isNaN(choice.value)) {
+        alert("Entrez un chiffre")
+    } else {
+        totalSeconds = choice.value * 60;
+        choice.value = "";
+        clearInterval(interval);
+        interval = setInterval (countDown, 1000);
+        }
+    })
+    // });    
